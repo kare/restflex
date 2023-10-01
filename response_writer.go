c9812f707bcb69250809f456ee9aaf2f31099851
Kare Nuorteva <kare.nuorteva@me.com>
2023-10-01 18:30:21 +0300

    docs: add godocs to responseWriter

--- response_writer.go
+++ response_writer.go
@@ -10,12 +10,16 @@ type responseWriter struct {
 	status    int
 }
 
+// WriteHeader calls normal http.ResponseWriter.WriteHeader() to set the status and
+// sets variable isWritten to true.
 func (w *responseWriter) WriteHeader(status int) {
 	w.ResponseWriter.WriteHeader(status)
 	w.status = status
 	w.isWritten = true
 }
 
+// Write calls http.ResponseWriter.Write() to write given bytes and sets
+// variable isWritten to true.
 func (w *responseWriter) Write(b []byte) (int, error) {
 	i, err := w.ResponseWriter.Write(b)
 	w.isWritten = true
